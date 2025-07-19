81253f144a5bc0f866e33b32b31863f37c3daa6c
adnan 275 <adnan.rizvi2004@gmail.com>
2025-07-20 01:02:36 +0530

    project final commit

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,15 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+
 const firebaseConfig = {
-  apiKey:process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain:process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket:process.env.REACT_APP_FIREBASE_STORAGEBUCKET,  
-  messagingSenderId:process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId:process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId:process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCR3hy8uJxPuJjg8iUR5Kvz_TZRiCPTHCQ",
+  authDomain: "taskly-22aff.firebaseapp.com",
+  projectId: "taskly-22aff",
+  storageBucket: "taskly-22aff.firebasestorage.app",
+  messagingSenderId: "466155352635",
+  appId: "1:466155352635:web:187138ca819737b543221e",
+  measurementId: "G-KY3Y4H3MJT"
 };
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export default app;
